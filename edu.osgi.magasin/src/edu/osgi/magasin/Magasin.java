4c69ea9ccb72740782afba72e63c8eb4a21defb4
Unknown <ale23@hotmail.fr>
2019-06-17 16:23:47 +0200

    Interface magasin ajouter

--- edu.osgi.magasin/src/edu/osgi/magasin/Magasin.java
+++ edu.osgi.magasin/src/edu/osgi/magasin/Magasin.java
@@ -1,7 +1,26 @@
 package edu.osgi.magasin;
 
-public class Magasin {
+public class Magasin implements Imagasin{
 	protected Panier panier;
-	
-	
+
+
+	@Override
+	public void ajoutProduit(String libelle, int quantite) {
+		
+	}
+
+	@Override
+	public double getPrixPanier() {
+		return 0;
+	}
+
+	@Override
+	public double envoisFacture() {
+		return 0;
+	}
+
+	@Override
+	public boolean produitDisponible(int idP, int quant) {
+		return false;
+	}
 }
