4ecdfcd59502e50229c1b85f18826b8948e3ed1d
tigerIonic <ceuterickx.gaetan@gmail.com>
2019-06-17 15:55:32 +0200

    maj stock

---
+++ edu.osgi.stock/src/edu/osgi/stock/ErreurProduit.java
@@ -0,0 +1,7 @@
+package edu.osgi.stock;
+
+public class ErreurProduit extends Exception {
+	public ErreurProduit(String m){
+		m.toString();
+	}
+}
--- edu.osgi.stock/src/edu/osgi/stock/Stock.java
+++ edu.osgi.stock/src/edu/osgi/stock/Stock.java
@@ -1,5 +1,45 @@
 package edu.osgi.stock;
 
-public class Stock {
+import java.util.Collection;
 
+public class Stock {
+	private Collection<Produit> stock;
+	
+	public Stock(){
+		
+	}
+	
+	public Stock(Collection<Produit> st){
+		this.stock=st;
+	}
+	
+	public void addProduit(Produit p) throws ErreurProduit{
+		
+		if (produitPresent(p)){
+			throw new ErreurProduit("produit déja présent");
+		}
+		else{
+			this.stock.add(p);
+		}
+	}
+
+	private boolean produitPresent(Produit p){
+		boolean estPresent=false;
+		for (Produit produit:stock){
+			if (produit.getId_produit()==p.getId_produit()){
+				estPresent=true;
+					
+				
+			}
+		}
+		return estPresent;
+	}
+	
+	public void addQuantite(Produit p,int quant) throws ErreurProduit{
+		if (produitPresent(p)){
+			p.setQuantite(p.getQuantite()+quant);
+		}else{
+			throw new ErreurProduit("produit non répertorié");
+		}
+	}
 }
