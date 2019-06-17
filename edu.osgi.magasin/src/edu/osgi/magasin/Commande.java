9e261b0801787f3668d166c4c2c08f9a06862e53
tigerIonic <ceuterickx.gaetan@gmail.com>
2019-06-17 16:19:54 +0200

    magasin

---
+++ edu.osgi.magasin/src/edu/osgi/magasin/Commande.java
@@ -0,0 +1,8 @@
+package edu.osgi.magasin;
+
+import edu.osgi.client.Client;
+
+public class Commande {
+	private Client client;
+	private Panier panier;
+}
---
+++ edu.osgi.magasin/src/edu/osgi/magasin/Magasin.java
@@ -0,0 +1,7 @@
+package edu.osgi.magasin;
+
+public class Magasin {
+	protected Panier panier;
+	
+	
+}
