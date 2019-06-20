dd8f929713abc21efd0c775a3415203f05d0fcb0
tigerIonic <ceuterickx.gaetan@gmail.com>
2019-06-20 19:13:37 +0200

    singleton stock

--- edu.osgi.stock/src/edu/osgi/stock/Activator.java
+++ edu.osgi.stock/src/edu/osgi/stock/Activator.java
@@ -15,10 +15,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
-		
-		initializeStock();
-		
+	public void start(BundleContext context) throws Exception {		
 		
 	}
 	
@@ -30,17 +27,5 @@ public class Activator implements BundleActivator {
 		
 	}
 	
-	private void initializeStock(){
-		Collection<Produit> collectionProduit = new ArrayList<Produit>();
-		collectionProduit.add(new Produit(1,"carotte",1.5,100));
-		collectionProduit.add(new Produit(2,"tomate",1.5,100));
-		collectionProduit.add(new Produit(3,"saucisson",3,100));
-		collectionProduit.add(new Produit(4,"navet",1,100));
-		collectionProduit.add(new Produit(5,"boeuf",12,100));
-		collectionProduit.add(new Produit(6,"lait",0.8,100));
-		collectionProduit.add(new Produit(7,"oeuf",0.2,1000));
-		Stock stock=new Stock(collectionProduit);
-		
-	}
-
+	
 }
--- edu.osgi.stock/src/edu/osgi/stock/Stock.java
+++ edu.osgi.stock/src/edu/osgi/stock/Stock.java
@@ -1,15 +1,33 @@
 package edu.osgi.stock;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import edu.osgi.api.ErreurProduit;
 import edu.osgi.api.Produit;
 
+
 public class Stock {
-	private Collection<Produit> stock;
+	private Collection<Produit> stock=new ArrayList<Produit>();
 	
-	public Stock(){
-		
+	/** Constructeur privé */
+	private Stock()
+	{
+		stock.add(new Produit(1,"carotte",1.5,100));
+		stock.add(new Produit(2,"tomate",1.5,100));
+		stock.add(new Produit(3,"saucisson",3,100));
+		stock.add(new Produit(4,"navet",1,100));
+		stock.add(new Produit(5,"boeuf",12,100));
+		stock.add(new Produit(6,"lait",0.8,100));
+		stock.add(new Produit(7,"oeuf",0.2,1000));
+	}
+
+	/** Instance unique pré-initialisée */
+	private static Stock INSTANCE = new Stock();
+
+	/** Point d'accès pour l'instance unique du singleton */
+	public static Stock getInstance()
+	{   return INSTANCE;
 	}
 	
 	public Stock(Collection<Produit> st){
